7bfd0261a37ea4c96552ae64331804c0789ac756
smitchaklasiya <126601131+smitchaklasiya@users.noreply.github.com>
2023-04-21 19:40:31 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+window.reviewData = [
+  {
+    name: "Jay",
+    date: "2023-03-15",
+    rating: 5,
+    text: "This game is fun to play with family. It's a fantastic way to spend time with someone."
+
+  },
+  {
+    name: "Abhishek",
+    date: "2020-10-05",
+    rating: 4,
+    text: "Monopoly is difficult and complex for me. Other games are superior."
+
+  },
+  {
+    name: "Maya",
+    date: "2022-05-18",
+    rating: 5,
+    text: "This is my absolute favourite game! It's a lot of fun to play with my pals."
+
+  },
+  {
+    name: "John",
+    date: "2019-02-07",
+    rating: 1,
+    text: "I despise this game! This game is terrible, and I don't think anyone should play it."
+
+  },
+  {
+    name: "Ryan",
+    date: "2020-12-29",
+    rating: 3,
+    text: "Even though the game takes a lot of time, playing with friends and family is enjoyable. "
+  }
+];
